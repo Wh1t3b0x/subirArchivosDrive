0da9294d0eb545923e3dea7c023c67b83d172ded
Wh1t3b0x <sebamolina5200@gmail.com>
2023-05-07 23:45:51 -0400

    Changes to be committed: 	new file:   recovery.py 	modified:   subirArchivosDrive.py

--- recovery.py
+++ recovery.py
@@ -3,14 +3,16 @@ import boto3
 import shutil
 import os
 import os.path
-import mimetypes
 from monday import MondayClient
 from botocore.exceptions import ClientError
 from google.auth.transport.requests import Request
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.oauth2.credentials import Credentials
-from googleapiclient.http import MediaFileUpload
 from googleapiclient.discovery import build
+
+''' al enviar informe y cambiar estado a informe enviado desde la columna "estado" de la tabla informe enviado, se desencadenara 
+y se subiran los archivos al drive de manera ordenada y en sus respectivas carpetas.
+'''
 
 def get_secret():
     secret_name = "Monday_API"
@@ -35,7 +37,6 @@ SCOPES = ["https://www.googleapis.com/auth/drive"]
 DISCOVERY_DOC = "https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"
 monday_api_key = get_secret()["monday_api"]
 mon = MondayClient(monday_api_key)
-columns_id = ["reflejo70", "archivo6", "archivo4", "archivo20", "dup__of_orden_de_compra", "archivo2", "archivo1"]
 
 if os.path.exists('token.json'):
     creds = Credentials.from_authorized_user_file('token.json', SCOPES)
@@ -62,21 +63,22 @@ def syncchallenge(event: dict):
 
 
 def lambda_handler(event, context):
+    # if challenge := syncchallenge(event):
+    #     return {
+    #         "isBase64Encoded": False,
+    #         "statusCode": 200,
+    #         "body": json.dumps(challenge),
+    #         "headers": {"content-type": "application/json"},
+    #     }
     
     evento = json.loads(event["body"])["event"]
     
     pulso_id = evento["pulseId"]
     columnas = mon.items.fetch_items_by_id(pulso_id)["data"]["items"][0]["column_values"]
-    #print (columnas)
-    url = None
-    datos = {}
+    print (columnas)
     for columna in columnas:
-        if columna["id"] in columns_id:
-            url = columna["text"]
-            #print("La url es: "+ url)
-            datos[columna["id"]] = url
-
-    #print(datos)
+        if columna["id"] == "dup__of_orden_de_compra":
+            print(columna["text"])
     
 #-------------------------------------------------------------------
     # Crear el objeto de servicio de la API de Google Drive
@@ -99,17 +101,6 @@ def lambda_handler(event, context):
         id_carpeta = carpeta['id']
         nombre_carpeta = carpeta['name']
         print("La carpeta '{}' fue encontrada con ID: {}".format(nombre_carpeta, id_carpeta))
-
-        # Iterar a través de todas las URL de los archivos y subirlos a la carpeta encontrada
-        for nombre_archivo, ruta_archivo in datos.items():
-            # Obtener el tipo de archivo a partir de la ruta del archivo
-            tipo_archivo, _ = mimetypes.guess_type(ruta_archivo)
-
-            # Subir el archivo a la carpeta encontrada
-            file_metadata = {'name': nombre_archivo, 'parents': [id_carpeta]}
-            media = MediaFileUpload(ruta_archivo, mimetype=tipo_archivo)
-            archivo = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
-            print("Se subió el archivo '{}' con ID: {}".format(nombre_archivo, archivo.get('id')))
     else:
         print("No se encontró la carpeta '{}'".format(nombre_carpeta))
         exit()
